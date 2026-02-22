8e74a48eedeff97466a376b4135829b1c5150419
Mike Dudley Ducasse <e2262669@cmaisonneuve.qc.ca>
2026-02-22 17:19:29 -0500

    firebase

---
+++ SpotC/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkYrWAD-NHEyboLlggWxqltfJFI3pJnWA",
+  authDomain: "spotc-a25c8.firebaseapp.com",
+  projectId: "spotc-a25c8",
+  storageBucket: "spotc-a25c8.firebasestorage.app",
+  messagingSenderId: "893479473393",
+  appId: "1:893479473393:web:0c3c86d16ae1204a449950",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
